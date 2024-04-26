8ecc3fb85dd35202b139bb7a5b1459ae353f7fda
ppaprikaa <sanzhar.maratov@proton.me>
2024-04-26 23:35:56 +0500

    main

--- main.c
+++ main.c
@@ -3,15 +3,12 @@
 #include "bitboard.h"
 
 int main(int argc, char* argv[argc + 1]) {
-	for(size_t rank = 0; rank < 8; rank++) {
-		for (size_t file = 0; file < 8; file++) {
-			size_t square = rank * 8 + file;
-			bitboard king_attack = bitboard_king_attack_mask(square);
-			bitboard_print(king_attack);
-			if (king_attack != king_attacks[square]) {
-				exit(1);
-			}
+	for (size_t r = 0; r < 8; r++) {
+	
+		for (size_t f = 0; f < 8; f++) {
+			bitboard_print(bitboard_bishop_attack_mask(r * 8 + f));
 		}
 	}
+
 	return EXIT_SUCCESS;
 }
