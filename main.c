f90ca7d671fdd6399392d8559fd27e9beddda0d0
ppaprikaa <sanzhar.maratov@proton.me>
2024-04-26 21:04:06 +0500

    main

--- main.c
+++ main.c
@@ -1,22 +1,18 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "bitboard.h"
-#include "bit_gears.h"
 
 int main(int argc, char* argv[argc + 1]) {
-	bitboard board = 1ULL | 2ULL;
-
-	PUT_BIT(board, a8);
-	PUT_BIT(board, b8);
-	bitboard_print(board);
-	getchar();
-
-	POP_BIT(board, b8);
-	bitboard_print(board);
-	getchar();
-
-	bitboard pawn_attacks_mask = bitboard_pawn_attack_mask(e2, white);
-	bitboard_print(pawn_attacks_mask);
+	for (size_t i = 0; i < 8; i++) {
+		for (size_t j = 0; j < 8; j++) {
+			bitboard square = i * 8 + j;
+			bitboard knight_attacks_mask = bitboard_knight_attack_mask(square);
+			bitboard_print(knight_attacks_mask);
+			if (knight_attacks_mask != knight_attacks[square]) {
+				exit(1);
+			}
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
