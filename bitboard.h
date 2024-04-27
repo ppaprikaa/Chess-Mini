c5892cd51ffc1a2ffd35c23f8de6769cab973ee4
ppaprikaa <sanzhar.maratov@proton.me>
2024-04-27 15:37:35 +0500

    better

--- bitboard.h
+++ bitboard.h
@@ -4,10 +4,10 @@
 
 void bitboard_print(bitboard board);
 
-bitboard bitboard_pawn_attack_mask(unsigned long long square, int side);
-bitboard bitboard_knight_attack_mask(unsigned long long square);
-bitboard bitboard_king_attack_mask(unsigned long long square);
+bitboard bitboard_pawn_attack_mask(bitboard square, int side);
+bitboard bitboard_knight_attack_mask(bitboard square);
+bitboard bitboard_king_attack_mask(bitboard square);
 
 // note: these are return relevant occupancy bits so file a and h and rank 1 and 8 will not have bits set to 1
-bitboard bitboard_bishop_attack_mask(unsigned long long square);
-bitboard bitboard_rook_attack_mask(unsigned long long square);
+bitboard bitboard_bishop_attack_mask(bitboard square);
+bitboard bitboard_rook_attack_mask(bitboard square);
