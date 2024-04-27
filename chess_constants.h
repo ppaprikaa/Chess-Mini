e67fc33b2284f1319a64e264d521304e29429629
ppaprikaa <sanzhar.maratov@proton.me>
2024-04-28 02:41:15 +0500

    fix typo

--- chess_constants.h
+++ chess_constants.h
@@ -33,7 +33,7 @@ enum {
 };
 
 // index to chess coordinate table
-const char cordinates[64][3] = {
+const char coordinates[64][3] = {
 	"a8","b8","c8","d8","e8","f8","g8","h8",
 	"a7","b7","c7","d7","e7","f7","g7","h7",
 	"a6","b6","c6","d6","e6","f6","g6","h6",
