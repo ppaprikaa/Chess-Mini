9275d0a4522b1e1b6b9003b399176e3a80fc513d
ppaprikaa <sanzhar.maratov@proton.me>
2024-05-01 23:27:09 +0500

    pseudo random number generator

---
+++ random.c
@@ -0,0 +1,15 @@
+#include "random.h"
+#include "stdio.h"
+
+unsigned int rand = 1681692777;
+
+unsigned int uint_random() {
+	unsigned int num = rand;	
+
+	num ^= num << 13;	
+	num ^= num << 13;	
+	num ^= num << 13;	
+	
+	rand = num;
+	return num;
+}
